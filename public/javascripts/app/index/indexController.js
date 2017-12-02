b63f6b206c1322eef91f1c865d4f52ebccbe0a64
Renan Hidemi Kataoka <rkataoka.edu@gmail.com>
2017-12-01 23:56:11 -0200

    Created Google Maps widget

--- public/javascripts/app/index/indexController.js
+++ public/javascripts/app/index/indexController.js
@@ -1,16 +1,25 @@
 angular.module("indexModule").controller("indexController", indexController);
 
+// Dependency injection
 indexController.$inject = ['$scope', '$timeout', '$window', 'datumApiService', 'datumApiMockService', 'globalVarsService'];
 
-// Start of Index Controller
+// Controller
 function indexController($scope, $timeout, $window, datumApiService, datumApiMockService, globalVarsService) {
 
-  // Upon load methods
+  // ENV variables
   var development_env = 'development';
   var use_mock = false;
   var defaultService = datumApiService;
-  $scope.cities = [{name: 'Selecione o estado'}];
 
+  // Scope variables
+  $scope.loadingWheel = false;
+  $scope.cities = [{name: 'Selecione o estado'}];
+  $scope.city_name = 'Selecione uma Cidade';
+  // $scope.city_ws = 'lll';
+
+  // --------------------------------
+  // Upon load methods
+  // --------------------------------
   globalVarsService.env().then(function(response) {
     var env_vars = response.data.ENV;
     development_env = env_vars.ENV;
@@ -25,8 +34,11 @@ function indexController($scope, $timeout, $window, datumApiService, datumApiMoc
       $scope.$apply();
     });
   });
+  // --------------------------------
 
+  // --------------------------------
   // Reactive functions
+  // --------------------------------
   $scope.filterByState = function(stateId) {
     defaultService.getCitiesByFederalUnit(stateId).then(function(response) {
       $scope.cities = response.city;
@@ -34,9 +46,102 @@ function indexController($scope, $timeout, $window, datumApiService, datumApiMoc
     })
   }
 
-  $scope.getCityAvailableData = function(cityId) {
-    defaultService.getAvailableDataFromCity(cityId).then(function(response) {
-      console.log(response);
+  $scope.getCityAvailableData = function(city) {
+    turnLoadingWheelOn()
+    refreshMap([city.latitude, city.longitude], city.name)
+    defaultService.getAvailableDataFromCity(city.id).then(function(response) {
+      handleCityData(response);
+      turnLoadingWheelOff();
+      $scope.$apply();
     })
   }
+
+
+  // --------------------------------
+  // Auxiliary methods
+  // --------------------------------
+  // - Handles city data upon loading
+  handleCityData = function(data) {
+    console.log(data);
+
+    if(data.weather_station_data.has_own_weather_station){
+      addWSMarkerOnMap(false, undefined, data.weather_station_data.weather_station_name)
+    }
+    else{
+      var latitude = data.weather_station_data.reference_city_latitude
+      var longitude = data.weather_station_data.reference_city_longitude
+
+      var coordinates = [latitude, longitude]
+
+      addWSMarkerOnMap(true, coordinates, data.weather_station_data.weather_station_name);
+      $scope.distanceBetweenCityAndWS = data.weather_station_data.distance;
+    }
+
+    $scope.hasNativeCommodities = data.has_available_native_commodities;
+    $scope.hasCommonProducers = data.has_available_common_producers;
+    $scope.hasSuggestedCommodities = data.has_available_common_producers;
+
+    // renderCityDataButtons();
+  }
+
+  // - Loading wheel handling
+  turnLoadingWheelOff = function() {
+    $scope.loadingWheel = false
+  }
+
+  turnLoadingWheelOn = function() {
+    $scope.loadingWheel = true
+  }
+
+  // - GMaps Refresh
+  refreshMap = function(coordinates=[-25.363, 131.044], label, zoom=8) {
+    var latitude = coordinates[0];
+    var longitude = coordinates[1];
+
+    $scope.city_name = label;
+    $scope.city_ws = '';
+
+    var uluru = { lat: latitude, lng: longitude };
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: zoom,
+      center: uluru
+    });
+    var marker = $scope.cityMarker = new google.maps.Marker({
+      position: uluru,
+      map: map
+    });
+  }
+
+  // - GMaps Add Marker
+  addWSMarkerOnMap = function(addMarker=false, coordinates=[-24.363, 130.044], label) {
+    $scope.city_ws =" (" + label + ")";
+
+    if(addMarker){
+      $scope.city_ws_color = 'red'
+
+      var latitude = coordinates[0];
+      var longitude = coordinates[1];
+
+      var uluru = { lat: latitude, lng: longitude };
+
+      var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 8,
+        center: uluru
+      });
+
+      var marker_1 = new google.maps.Marker({
+        position: $scope.cityMarker.position,
+        map: map
+      });
+
+      var marker_2 = new google.maps.Marker({
+        position: uluru,
+        map: map
+      });
+    } else {
+      $scope.city_ws_color = 'green'
+    }
+  }
+  // --------------------------------
 }
